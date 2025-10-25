44f157980148c83c7a432d7e4f7da0eb8cf34103
Maneesh Parihar <maneeshparihar@gmail.com>
2025-10-25 10:07:35 +0000

    fix: adding supervisor

--- python/agents/customer-service/deployed-agent-demo/run_deployed_agent_be.sh
+++ python/agents/customer-service/deployed-agent-demo/run_deployed_agent_be.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 export GOOGLE_CLOUD_PROJECT=project1-193817
 export GOOGLE_CLOUD_LOCATION=us-central1 
-uv run python ./web_backend.py
+/home/maneeshparihar/.local/bin/uv run python ./web_backend.py
